4544d3d4190f84202ba98c4d857b907369ae8279
DarkoPG27 <darkobrnovic27@gmail.com>
2021-12-06 17:12:31 +0100

    Adding a Responsive Form

--- src/Form.js
+++ src/Form.js
@@ -11,16 +11,42 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import MenuItem from '@material-ui/core/MenuItem';
-import Selectr from '@material-ui/core/Select';
+import Select from '@material-ui/core/Select';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 import styles from './styles/FormStyles';
 
 class Form extends Component {
     render() {
+        const { classes } = this.props;
         return (
             <main className={classes.main} >
+                <Paper className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography variant='h5'> Sign In </Typography>
+                    <Select value="english">
+                        <MenuItem value="english">English</MenuItem>
+                        <MenuItem value="serbian">Serbian</MenuItem>
+                        <MenuItem value="russian">Russian</MenuItem>
+                    </Select>
+                    <form className={classes.form}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email</InputLabel>
+                            <Input id="email" name="email" autoFocus></Input>
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" name="password" autoFocus></Input>
+                        </FormControl>
+                        <FormControlLabel control={<Checkbox color="primary" />} label="Remeber Me" />
+                        <Button variant="contained" type="submit" fullWidth color="primary" className={classes.submit}>Sign In</Button>
 
+
+
+                    </form>
+                </Paper>
             </main>
         )
     }
